712c7a998a119f07d46d59fb36d5eda41c50caae
Spandan1460 <spandan.chakravarty@maersk.com>
2024-10-11 05:23:43 +0530

    Test Artifact

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Build each component individually
-docker rmi -f oct:v1.0.0
-docker build -t oct:v1.0.0 .
-if [[ $? -eq 0 ]]; then
-	./run-local.sh
-	docker image list
-fi
--- run-local.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run --rm -it --env ASPNETCORE_ENVIRONMENT=Development --hostname oct --name oct -p 8000:8080 oct:v1.0.0
